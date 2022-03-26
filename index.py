2b15f7b78a976ed231be5574dc13b5df1ff7972b
Douglas <contrerasdouglas2001@gmail.com>
2022-03-26 00:11:40 -0600

    changes 2

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from app import app
+
+from db.db import landingdb
+
 #from utils.db import db
 
-# with app.app_context():
-    # db.create_all()
+with app.app_context():
+    landingdb.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
